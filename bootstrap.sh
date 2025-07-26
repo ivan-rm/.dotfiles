a0dbda9cc1abb27af33b92f94262a92174c61b7c
Iván Ruiz Marcos <ivanrm@norabide.com>
2025-07-26 19:37:14 +0200

    Install nerd fonts - JetBrainsMono

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ sudo apt upgrade
 flatpak update
 
 sudo apt install -y curl trurl eza fd-find bat zoxide ripgrep fzf tldr jq vlc htop
-sudo apt install -y build-essential gdb valgrind make neovim fish stow
+sudo apt install -y build-essential gdb valgrind make neovim fish stow plocate
 #sudo aptinstall -y fonts-cascadia-code fonts-jetbrains-mono
 sudo apt install -y wireshark nmap virt-manager bind9-dnsutils
 #sudo apt install lazygit    # When it becomes available
@@ -28,6 +28,16 @@ flatpak install -y flathub org.gaphor.gaphor
 # Enable the firewall
 sudo ufw enable
 sudo ufw status
+
+#Install fonts
+mkdir -p ~/.local/share/fonts
+
+cd /tmp
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+unzip JetBrainsMono.zip -d JetBrainsMono
+cp JetBrainsMono/*.ttf ~/.local/share/fonts
+rm -rf JetBrainsMono.zip JetBrainsMono
+
 
 # Clone the dotfiles repo and use stow to symlink the config in place
 git clone git@github.com:ivan-rm/.dotfiles.git
